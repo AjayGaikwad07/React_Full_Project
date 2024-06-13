18d21f703be37729da1ca38849fa4f669d4a839b
Ajeight <ajaygaikwad3131@gmail.com>
2024-06-13 12:34:38 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// Routing
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
